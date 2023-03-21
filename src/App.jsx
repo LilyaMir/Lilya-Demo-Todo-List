8ba0d01d7a230df8693300333ae370dd21756fbb
LilyaMir <mirzoyan.lilya.am@gmail.com>
2023-03-21 16:20:09 +0400

    lesson-64

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Todo from './components/Todo';
-
+import Todo from './components/todo/Todo';
 import './App.css';
 
 function App(){
     return (
-      <main>
-          <Todo />
-      </main>
-
+    <Todo />
     );
 }
 
