ff6395cbab72718350304d6894944c2e7d1ad812
LilyaMir <mirzoyan.lilya.am@gmail.com>
2023-04-27 12:13:55 +0400

    Lesson-74

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -1,12 +1,13 @@
-import { memo } from 'react';
-import { Modal, Button } from "react-bootstrap";
+import {memo} from 'react';
+import {Modal, Button} from "react-bootstrap";
 import PropTypes from 'prop-types';
 
 function ConfirmDialog(props) {
   return (
     <Modal size="md" show={true} onHide={props.onCancel}>
       <Modal.Header closeButton>
-        <Modal.Title>Are you sure to delete {props.tasksCount !== 0 ? props.tasksCount : null} {props.tasksCount > 1 ? 'tasks' : 'task'}?</Modal.Title>
+        <Modal.Title>
+          Are you sure to delete {props.tasksCount !== 0 ? props.tasksCount : null} {props.tasksCount > 1 ? 'tasks' : 'task'}?</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <div className="d-flex justify-content-evenly">
