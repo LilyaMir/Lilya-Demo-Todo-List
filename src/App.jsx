9d47b003930c58e28a98e62d2fac3f168316ffe8
LilyaMir <mirzoyan.lilya.am@gmail.com>
2023-05-13 10:39:56 +0400

    Lesson-77

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,66 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
-import 'react-toastify/dist/ReactToastify.css';
-import Todo from './components/todo/Todo';
-import './App.css';
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Todo from "./pages/todo/Todo";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import NavBar from "./components/navBar/NavBar"
+import SingleTask from "./pages/singleTask/SingleTask";
+import NotFound from './pages/notFound/NotFound';
 
-function App(){
-    return (
-      <main className="body main">      
-          <Todo />
+
+const pages = [
+  {
+    path: "/",
+    element: <Todo />,
+  },
+  {
+    path: "/todo",
+    element: <Todo />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/task/:taskId",
+    element: <SingleTask />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+function App() {
+  return (
+    <BrowserRouter>
+      <main>
+        <NavBar />
+        <Routes>
+          {
+            pages.map(page => (
+              <Route
+                key={page.path}
+                path={page.path}
+                element={page.element}
+              />
+            ))
+          }
+        </Routes>
       </main>
-
-    );
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/todo/Todo.jsx
+++ src/pages/todo/Todo.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import Task from "../task/Task";
-import ConfirmDialog from "../ConfirmDialog";
-import DeleteSelected from "../deleteSelected/DeleteSelected";
-import TaskModal from "../taskModal/TaskModal";
-import Filters from "../filters/Filters";
+import Task from "../../components/task/Task";
+import ConfirmDialog from "../../components/ConfirmDialog";
+import DeleteSelected from "../../components/deleteSelected/DeleteSelected";
+import TaskModal from "../../components/taskModal/TaskModal";
+import Filters from "../../components/filters/Filters";
 import TaskApi from "../../api/taskApi";
 import styles from "./todo.module.css";
 
